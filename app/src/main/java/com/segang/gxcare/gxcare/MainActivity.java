90ad8a6ea691e0b6bd4e3b590de1889f42297d9f
Merek Orlowski <merekorlowski@gmail.com>
2018-01-28 01:18:57 -0500

    Implemented fragments for navigation

--- app/src/main/java/com/segang/gxcare/gxcare/MainActivity.java
+++ app/src/main/java/com/segang/gxcare/gxcare/MainActivity.java
@@ -3,31 +3,38 @@ package com.segang.gxcare.gxcare;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                    // Create new fragment and transaction
+                    selectedFragment = Home.newInstance();
+                    break;
+                case R.id.navigation_schedule:
+                    // Create new fragment and transaction
+                    selectedFragment = Schedule.newInstance();
+                    break;
+                case R.id.navigation_profile:
+                    // Create new fragment and transaction
+                    selectedFragment = Profile.newInstance();
+                    break;
             }
-            return false;
+
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout, selectedFragment);
+            transaction.commit();
+            return true;
         }
     };
 
@@ -36,9 +43,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+
+        //Manually displaying the first fragment - one time only
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, Home.newInstance());
+        transaction.commit();
+
     }
 
 }
